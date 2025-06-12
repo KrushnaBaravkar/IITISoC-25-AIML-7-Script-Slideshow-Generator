3724729d5bac530c7684aad2305435550830a6e5
aryan6336 <aryankumar74278@gmail.com>
2025-06-12 10:45:08 +0530

    Update llama_agent.py

--- app/llama_agent.py
+++ app/llama_agent.py
@@ -1,7 +1,6 @@
 import openai
 import streamlit as st
 
-OPENAI_API_KEY = "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"
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 def generate_script_with_openai(prompt):
